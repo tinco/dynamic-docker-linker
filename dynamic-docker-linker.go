b580b4e52036392c6dd3561f2af20f8bd899a6df
Tinco Andringa <mail@tinco.nl>
2014-07-04 01:31:02 +0200

    add comments to understand what's happening

--- dynamic-docker-linker.go
+++ dynamic-docker-linker.go
@@ -137,9 +137,30 @@ func updateNamespaceNetworkLinks(name string, sourceAddr *net.IPAddr, ports io.R
 		return err
 	}
 
+	/*
+
+These would be the commands that they fabricate.
+
+iptables -t nat -A PREROUTING -d ${local_ip}/32 -p tcp -m tcp --dport ${local_port} -j DNAT --to-destination ${remote_ip}:${remote_port}
+
+iptables -t nat -A OUTPUT -d ${local_ip}/32 -p tcp -m tcp --dport ${local_port} -j DNAT --to-destination ${remote_ip}:${remote_port}
+
+iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source ${container_ip}
+
+So basically what GearD does is store the list of iptable rules, and restore them everytime something changes.
+
+Ideally we'd have this command be stateless. So what we could do is first run iptables-save, load it into a datastructure, add our
+new link to that datastructure, replacing any previous link on the same interface+port, and serializing that out to iptables-restore.
+
+	*/
+
+
 	fmt.Fprintf(stdin, "*nat\n")
 	for {
 		link := containers.NetworkLink{}
+		// this Fscanf call reads a line, setting the fromHost FromPort ToPort and ToHost on the link structure.
+		// if it encounters EOF, it will break out of the for loop
+		// so we could go without the for loop, and just make this link structure from the link argument
 		if _, err := fmt.Fscanf(ports, "%s\t%v\t%v\t%s\n", &link.FromHost, &link.FromPort, &link.ToPort, &link.ToHost); err != nil {
 			if err == io.EOF {
 				break
@@ -147,11 +168,16 @@ func updateNamespaceNetworkLinks(name string, sourceAddr *net.IPAddr, ports io.R
 			log.Printf("gear: Could not read from network links file: %v", err)
 			continue
 		}
+		// Apparently a link can be invalid.
 		if err := link.Check(); err != nil {
 			log.Printf("gear: Link in file is not valid: %v", err)
 			continue
 		}
+		// What could link.Complete() be?
 		if link.Complete() {
+			// This is fancy, it checks whether the hosts are resolvable
+			// Since we're going to link only local ip's (which might proxy to remote machines)
+			// We might as well skip the whole hosts step and work only with ips
 			srcIP, err := net.ResolveIPAddr("ip", link.FromHost)
 			if err != nil {
 				log.Printf("gear: Link source host does not resolve %v", err)
@@ -166,6 +192,8 @@ func updateNamespaceNetworkLinks(name string, sourceAddr *net.IPAddr, ports io.R
 
 			log.Printf("Mapping %s(%s):%d -> %s:%d", sourceAddr.String(), srcIP.String(), link.FromPort, destIP.String(), link.ToPort)
 
+			// It builds the data and then puts it into a template which gets output to stdin.
+			// we need to get that template
 			data := OutboundNetworkIptables{sourceAddr.String(), srcIP.IP.String(), link.FromPort, destIP.String(), link.ToPort}
 			if err := OutboundNetworkIptablesTemplate.Execute(stdin, &data); err != nil {
 				log.Printf("gear: Unable to write network link rules: %v", err)
